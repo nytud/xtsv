6173e7f7780e8a4a3f1fdfe11e7573eb3c8ea22d
mittelholcz <dev.mittelholcz@gmail.com>
2019-11-21 18:07:36 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='xtsv',
+    version='0.0.1',
+    author='dlazesz',
+    author_email='devel@oliphant.nytud.hu',
+    description='A generic TSV-style format based intermodular communication framework and REST API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dlt-rilmta/xtsv',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: LGPL3.0 License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
